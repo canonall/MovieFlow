68ae53b8649a87eb20c53c693a407e8e7cff8974
Can Önal <45923198+canonall@users.noreply.github.com>
2022-02-22 17:23:35 +0300

    Bug/request fail screen (#4)
    * ProgressBar added
    
    * UI logic for Success, Error, Loading added
    
    * PopularMovieListStateStatus added

--- app/src/main/java/com/canonal/movie/ui/movieHome/PopularMovieListState.kt
+++ app/src/main/java/com/canonal/movie/ui/movieHome/PopularMovieListState.kt
@@ -6,4 +6,9 @@ data class PopularMovieListState(
     val movieList: List<Movie> = emptyList(),
     val errorMessage: String? = "",
     val isLoading: Boolean = true,
+    val status: PopularMovieListStateStatus = PopularMovieListStateStatus.LOADING
 )
+
+enum class PopularMovieListStateStatus {
+    SUCCESS, ERROR, LOADING
+}
