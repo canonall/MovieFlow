da5b748425802d40eca6ce8d2bb24bfaff8bd031
canonal <onalcan20@gmail.com>
2022-02-22 15:22:39 +0100

    UI logic for Success, Error, Loading added

--- app/src/main/java/com/canonal/movie/ui/movieHome/HomeMovieFragment.kt
+++ app/src/main/java/com/canonal/movie/ui/movieHome/HomeMovieFragment.kt
@@ -1,9 +1,8 @@
 package com.canonal.movie.ui.movieHome
 
-import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.canonal.movie.R
 import com.canonal.movie.databinding.FragmentHomeMovieBinding
 import com.canonal.movie.di.AppModule
 import com.canonal.movie.ui.base.BaseFragment
@@ -20,23 +19,45 @@ class MovieHomeFragment :
 
     override fun initUi() {
         homeMovieViewModel.getPopularMovies()
+        setPopularMovieObserver()
+    }
+
+    private fun setPopularMovieObserver() {
         collectLatestLifecycleAwareStateFlow(
             homeMovieViewModel.popularMovieListState
         ) { popularMovieListState ->
-
-            if (popularMovieListState.movieList.isEmpty().not()) {
-                initializeRecyclerView(
-                    binding.rvPopularMovie,
-                    PopularMovieAdapter(popularMovieListState.movieList),
-                    LinearLayoutManager(requireContext())
-                )
-            } else {
-                binding.tvError.text = popularMovieListState.errorMessage
-                binding.tvError.visibility = View.VISIBLE
-                binding.ivError.visibility = View.VISIBLE
+            when (popularMovieListState.status) {
+                PopularMovieListStateStatus.SUCCESS -> {
+                    clearUi()
+                    initializeRecyclerView(
+                        binding.rvPopularMovie,
+                        PopularMovieAdapter(popularMovieListState.movieList),
+                        LinearLayoutManager(requireContext())
+                    )
+                }
+                PopularMovieListStateStatus.ERROR -> {
+                    loadErrorUi(popularMovieListState)
+                }
+                PopularMovieListStateStatus.LOADING -> {
+                    binding.progressBar.isVisible = true
+                }
             }
-
         }
+    }
+
+    private fun loadErrorUi(popularMovieListState: PopularMovieListState) {
+        binding.rvPopularMovie.isVisible = false
+        binding.progressBar.isVisible = false
+        binding.tvError.text = popularMovieListState.errorMessage
+        binding.tvError.isVisible = true
+        binding.ivError.isVisible = true
+    }
+
+    private fun clearUi() {
+        binding.rvPopularMovie.isVisible = true
+        binding.tvError.isVisible = false
+        binding.ivError.isVisible = false
+        binding.progressBar.isVisible = false
     }
 
 
